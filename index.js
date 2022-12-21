3a5cc52488887a7672b12f1eb8deeddec72ca25c
Melannycer <115270203+Melannycer@users.noreply.github.com>
2022-12-21 17:44:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+    rollDice();
+}
+// first, we have this line that declares that when we first enter the page it should show "Refresh me!" text
+
+function rollDice() {
+// we declare the function that will appear once you have hit the reload button
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 // this is the main operation to set random numbers
 var randomDiceImage = "dice" + randomNumber1 + ".png";
@@ -14,16 +21,22 @@ var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
 // then, we add the source with the random dice number for the second dice
 document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
 // first we select the img of second dice and change the attribute of source for the one that we created again
-if (randomNumber1 > randomNumber2) {
+changeTitle(randomNumber1, randomNumber2);
+//we declare that randomNumbers1 and 2 are the same as num1 and 2
+}
+
+function changeTitle(num1, num2) {
+if (num1 > num2) {
 // this is a condition that set a winner between first and second player, in this case if player 1 has a bigger number than 2 then...
     document.querySelector('h1').innerHTML="🎉 Player 1 Wins!";
 // select the title and change its content to this text
-  } else if (randomNumber1 < randomNumber2) {
+} else if (num1 < num2) {
 // if not, check if happens the opposite situation and do the next...
     document.querySelector("h1").innerHTML="Player 2 Wins! 🎉";
 // select the title and change its content to this another text
-  } else if (randomNumber1 === randomNumber2) {
+} else if (num1 === num2) {
 // if both of them are equal then...
     document.querySelector('h1').innerText="🎉 Draw! 🎉";
-// select the title and change its content to this last text
+// select the title and change its content to this text
   }
+}
